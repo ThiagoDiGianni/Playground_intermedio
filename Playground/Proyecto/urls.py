be581c16351599efb016c7fa8cdb46d87958970a
ThiagoDigianni <thiagodigianni@hotmail.com>
2022-05-15 17:02:58 -0300

    cuarto commit

--- Playground/Proyecto/urls.py
+++ Playground/Proyecto/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from person.views import *
+from Apps.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('persona',list_person),
     path('modelos', papa),
+    path("person",include("person.urls")),
     
 ]
