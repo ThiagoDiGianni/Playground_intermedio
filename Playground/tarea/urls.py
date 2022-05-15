019082a2808d1a5f101e5d7d86f276ee900169ec
ThiagoDigianni <thiagodigianni@hotmail.com>
2022-05-15 17:26:44 -0300

    commit nuevo

--- Playground/tarea/urls.py
+++ Playground/tarea/urls.py
@@ -1,12 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from Apps.views import *
+from person.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('persona',list_person),
     path('modelos', papa),
-    path("person",include("person.urls")),
     
 ]
